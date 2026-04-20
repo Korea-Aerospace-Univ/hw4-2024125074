7b3e95b3d67ead2c0f283b190627006e7c39573b
2024125074 <gayoun0805@naver.com>
2026-04-20 16:06:57 +0900

    Refactor guessing game logic and output
    Updated the output message for correct guess and added attempt count display.

--- p1/main.c
+++ p1/main.c
@@ -11,14 +11,14 @@ int main(void)
         scanf("%d",&a); //정답을 시도할 변수 정수형으로 입력 받기
         count++; //정답 시도 횟수 +1
         if (s==a) { //정답이라면
-            printf("%d==%d",a,s); 
+            printf("%d==?",a); 
+            printf("\n%d",count); //시도횟수 출력
         } else if (a>s) { //입력한 변수가 정답보다 크다면
             printf("%d>?\n",a);
         } else { //입력한 변수가 정답보다 작다면
             printf("%d<?\n",a); 
         }
-    } while (count <5 && a!= s); //while문이 참이면 do에 있는 문장을 반복하고
-         printf("\n%d",count); //while문이 거짓이면 출력
+    } while (a!= s); //정답이랑 입력 변수가 같을 때까지 반복
 
     return 0;
 }
