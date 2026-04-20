adeb7248964586fb25b5a3bec87685cb036694a3
2024125074 <gayoun0805@naver.com>
2026-04-20 16:44:47 +0900

    Implement string analysis for letters and digits
    This program reads a string and counts the maximum consecutive occurrences of lowercase letters and digits.

---
+++ p2/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{   
+    int n=0, countE=0,countN=0,countEMax=0,countNmax=0; //n:입력받을 문자열의 수
+    char a[101];
+    
+    
+    scanf("%d",&n);
+    scanf("%s",a);
+    
+    for (int i=0; i<n; i++) {
+        char now =a[i];
+        
+        if (now >= 'a' && now <='z') {
+            countE++;
+            if (countEMax < countE ) {
+                countEMax = countE;
+            }    
+        } else {
+            countE=0;
+        }
+        if (now >= '0' && now <='9') {
+            countN++;
+            if (countN >countNmax) {
+                countNmax = countN;
+            }
+        } else {
+            countN=0;
+        }
+    }
+    printf("%d\n",countEMax);
+    printf("%d\n",countNmax);
+    
+    return 0;
+}
