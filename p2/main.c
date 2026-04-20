13162a443dc068f14a1827546bb6417a208ebf8b
2024125074 <gayoun0805@naver.com>
2026-04-20 21:14:24 +0900

    Refactor character input and counting logic
    Refactor main function to read characters one by one and count consecutive letters and digits.

--- p2/main.c
+++ p2/main.c
@@ -2,35 +2,34 @@
 
 int main(void)
 {   
-    int n=0, countE=0,countN=0,countEMax=0,countNmax=0; //n:입력받을 문자열의 수,
-    char a[101]; // 배열 쓰지 말고 하나하나 다 입력 받
+    int n=0, countE=0,countN=0; //n:입력될 문자의 개수,countE:현재 연속된 영어의 수,countN:현재 연속된 숫자의 수
+    int countEMax=0,countNmax=0; //countEMax:연속된 영어의 최댓값, countNmax:연속된 숫자의 최댓값
+    char a; //문자를 하나씩 읽기 위한 변수 a선언
     
+    scanf("%d",&n); //입력될 문자의 개수 입력 받기
     
-    scanf("%d",&n);
-    scanf("%s",a);
-    
-    for (int i=0; i<n; i++) {
-        char now =a[i];
+    for (int i=1; i<=n+1; i++) { //scanf 입력 후 \n이 포함하는 것을 막기 위해 i는 1부터 시작하고 입력될 문자의 개수만큼 반복해야하므로 n+1까지 1씩 증가하여 반복
+        scanf("%c",&a); //문자 한개를 입력 받아 a에 저장
         
-        if (now >= 'a' && now <='z') {
-            countE++;
-            if (countEMax < countE ) {
-                countEMax = countE;
+        if (a >= 'a' && a <='z') { //만약 입력 받은 a가 영어 소문자라면
+            countE++; //현재 연속된 영어의 수 +1 증가
+            if (countEMax < countE ) { //만약 현재 연속된 영어의 수가 연속된 영어의 최댓값보다 크다면
+                countEMax = countE; //최댓값에 현재 영어값 넣기
             }    
-        } else {
-            countE=0;
+        } else { //만약 영어 소문자가 없다면
+            countE=0; //현재 연속된 영어의 수는 0
         }
-        if (now >= '0' && now <='9') {
-            countN++;
-            if (countN >countNmax) {
-                countNmax = countN;
+        if (a >= '0' && a <='9') { //만약 입력 받은 a가 숫자라면
+            countN++; //현재 연속된 숫자의 수 +1 증가
+            if (countN >countNmax) { //만약 현재 연속된 숫자의 수가 연속된 숫자의 최댓값보다 크다면
+                countNmax = countN; //최댓값에 현재 숫자값 넣기
             }
-        } else {
-            countN=0;
+        } else { //만약 숫자가 없다면
+            countN=0; //현재 연속된 숫자의 수는 0
         }
     }
-    printf("%d\n",countEMax);
-    printf("%d\n",countNmax);
+    printf("%d\n",countEMax); //연속된 영어의 최댓값 출력
+    printf("%d\n",countNmax); //연속된 숫자의 최댓값 출력
     
     return 0;
 }
